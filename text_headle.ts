80136f4a98a4e50763b3a5813a172df01b655cda
xHak2215 <147624393+xHak2215@users.noreply.github.com>
2025-09-19 20:36:31 +0300

    Update text_headle.ts

--- text_headle.ts
+++ text_headle.ts
@@ -1,17 +1,22 @@
 import * as vscode from 'vscode';
-const command_list: string[] = require('./syntaxes/command_list.js'); 
+const command_list: string[] = require('./syntaxes/command_list.js');
 
 export class MySemanticTokensProvider implements vscode.DocumentSemanticTokensProvider {
     provideDocumentSemanticTokens(document: vscode.TextDocument): vscode.ProviderResult<vscode.SemanticTokens> {
         const tokensBuilder = new vscode.SemanticTokensBuilder();
         const text = document.getText();
         const lines = text.split(/\r?\n/);
-        console.info(lines+' оброботка текста')
+        console.info(lines + ' оброботка текста');
+
         lines.forEach((line, lineIndex) => {
             command_list.forEach(keyword => {
-                const index = line.indexOf(keyword);
-                if (index !== -1) {
+                // Используем регулярное выражение для проверки ключевого слова
+                const regex = new RegExp(`(?<!\\d)${keyword}(?!\\d)`);
+                const match = line.match(regex);
+                
+                if (match) {
                     // Найдите позицию двоеточия
+                    const index = match.index;
                     const colonIndex = line.indexOf(':', index);
                     if (colonIndex !== -1) {
                         // Подсвечиваем текст после двоеточия
@@ -21,10 +26,10 @@ export class MySemanticTokensProvider implements vscode.DocumentSemanticTokensPr
                         // Удаляем пробелы в начале текста после двоеточия
                         const trimmedText = line.substring(startIndex).trim();
                         const trimmedStartIndex = startIndex + (line.substring(startIndex).indexOf(trimmedText));
-
+                        console.info(lineIndex, trimmedStartIndex, lineIndex, endIndex)
                         tokensBuilder.push(
                             new vscode.Range(lineIndex, trimmedStartIndex, lineIndex, endIndex),
-                            'text' 
+                            'text'
                         );
                     }
                 }
