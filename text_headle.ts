4ca756bd752c7d07aa748cb96b7bf45e63618664
xHak2215 <147624393+xHak2215@users.noreply.github.com>
2025-09-19 10:00:47 +0300

    Add files via upload

---
+++ text_headle.ts
@@ -0,0 +1,39 @@
+import * as vscode from 'vscode';
+const command_list: string[] = require('./command_list.js'); 
+
+export function activate(context: vscode.ExtensionContext) {
+    const disposable = vscode.languages.registerDocumentSemanticTokensProvider(
+        { language: 'rwmodcode' }, 
+        new MySemanticTokensProvider(),
+        legend
+    );
+
+    context.subscriptions.push(disposable);
+}
+
+class MySemanticTokensProvider implements vscode.DocumentSemanticTokensProvider {
+    provideDocumentSemanticTokens(document: vscode.TextDocument): vscode.ProviderResult<vscode.SemanticTokens> {
+        const tokensBuilder = new vscode.SemanticTokensBuilder();
+        const text = document.getText();
+        const lines = text.split(/\r?\n/);
+
+        lines.forEach((line, lineIndex) => {
+            command_list.forEach(keyword => {
+                const index = line.indexOf(keyword);
+                if (index !== -1) {
+                    const startIndex = index + keyword.length + 1; 
+                    const endIndex = line.length;
+
+                    tokensBuilder.push(
+                        new vscode.Range(lineIndex, startIndex, lineIndex, endIndex),
+                        'text' 
+                    );
+                }
+            });
+        });
+
+        return tokensBuilder.build();
+    }
+}
+
+const legend = new vscode.SemanticTokensLegend(['text']);
